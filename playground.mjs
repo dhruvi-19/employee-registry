989f756b258ab28ce3075ceea7f976e2b9ae93f5
Dhruvi Bhuva <dhruvi.b@prometteur.in>
2024-07-23 12:00:11 +0530

    updated searchTerm using useEffect

--- playground.mjs
+++ playground.mjs
@@ -1,14 +1,3 @@
-import axios from "axios";
-
-const url = "http://localhost:4000/employees";
-
-// axios.get(url).then((response) => {
-//   console.log(response.data);
-// });
-
-const getEmployees = async () => {
-  const response = await axios.get(url);
-  console.log(response.data);
-};
-
-getEmployees();
+setTimeout(() => {
+  console.log("Hello from setTimeout");
+}, 3000);
--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ const App = () => {
   const [employees, setEmployees] = useState([]);
 
   useEffect(() => {
-    const url = "http://localhost:4000/employees";
-    axios.get(url).then((response) => {
+    const fetchEmployees = async () => {
+      const url = "http://localhost:4000/employees";
+      const response = await axios.get(url, { params: { q: searchTerm } });
       const employees = response.data;
       setEmployees(employees);
-    });
-  });
+    };
+
+    fetchEmployees();
+  }, [searchTerm]);
 
   return (
     <>
@@ -70,85 +73,5 @@ const App = () => {
     </>
   );
 };
-
-/*
-class App extends React.Component {
-  state = {
-    searchTerm: "",
-    employees: [],
-  };
-
-  async componentDidMount() {
-    const url = "http://localhost:4000/employees";
-    const response = await axios.get(url);
-    const employees = response.data;
-    this.setState({ employees });
-  }
-
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchTerm !== this.state.searchTerm) {
-      const url = "http://localhost:4000/employees";
-      const response = await axios.get(url, {
-        params: { q: this.state.searchTerm },
-      });
-      const employees = response.data;
-      this.setState({ employees });
-    }
-  }
-
-  handleEmployeeSearch = (event) => {
-    this.setState({ searchTerm: event.target.value });
-  };
-
-  render() {
-    const { searchTerm, employees } = this.state;
-
-    return (
-      <>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          <Box sx={{ bgcolor: "#cfe8fc", height: "100vh", padding: "2rem" }}>
-            <Typography variant="h1" align="center" sx={{ fontSize: "3rem" }}>
-              Employee Registry
-            </Typography>
-
-            <TextField
-              id="employeeName"
-              label="Employee Name"
-              value={searchTerm}
-              onChange={this.handleEmployeeSearch}
-              variant="standard"
-              margin="normal"
-              sx={{ width: "100%" }}
-            />
-
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-                padding: "32px 64px",
-                gap: "16px",
-              }}
-            >
-              {employees.map((employee) => (
-                <Card sx={{ width: "300px" }} key={employee.id}>
-                  <CardHeader
-                    avatar={<Avatar>{employee.initials}</Avatar>}
-                    title={employee.name}
-                  />
-                  <CardContent>
-                    <Typography>{employee.description}</Typography>
-                  </CardContent>
-                </Card>
-              ))}
-            </Box>
-          </Box>
-        </Container>
-      </>
-    );
-  }
-}
-*/
 
 export default App;
